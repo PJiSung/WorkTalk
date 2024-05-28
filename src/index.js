5711c00d871e4387be6cc8cfced693d03407c168
PJS <starcr222@naver.com>
2024-05-28 13:52:56 +0900

    login commit

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const createWindow = () => {
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 };
-
+ 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.

95c2d07070646230099e4155eac2eaf54812b7dc
PJS <starcr222@naver.com>
2024-05-31 03:18:30 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -14,7 +14,8 @@ expressApp.use(express.static(__dirname + '/public'));
 expressApp.use('/static', express.static(path.join(__dirname, 'node_modules')));
 
 expressApp.get('/', function (req, res) {
-    res.render('PJS/enroll')
+    //res.render('PJS/enroll')
+    res.render('index')
 })
 
 expressApp.listen(8080, function () {

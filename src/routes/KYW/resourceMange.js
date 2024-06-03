f2534df0405b376172365d11027ae6334e510392
Yongwoo Kim <james960506@naver,com>
2024-06-03 13:22:50 +0900

    update

--- src/routes/KYW/resourceMange.js
+++ src/routes/KYW/resourceMange.js
@@ -1,21 +1,44 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+
+const db = require('../../database.js'); // mongoose 설정 파일 경로에 맞게 변경하세요.
+const user = require('../../models/user.js'); // Employee 모델 파일 경로에 맞게 변경하세요.
+
 require('dotenv').config();
 
-router.get('/enroll', async (req, res) => {
-    res.render('KYW/enrollEmp.ejs')
+router.get('/asd', (req, res) => {
+    res.render('KYW/login.ejs')
 });
 
-router.get('/applicate', async (req, res) => {
+router.get('/applicate', (req, res) => {
     res.render('KYW/applicate.ejs')
 });
 
-router.get('/vConfirm', async (req, res) => {
-    res.render('KYW/vConfirm.ejs')
+router.get('/vCalendar', (req, res) => {
+    res.render('KYW/vCalendar.ejs')
 });
 
-router.get('/vManage', async (req, res) => {
+router.get('/vManage', (req, res) => {
     res.render('KYW/vManage.ejs')
+});
+
+router.get('/myChatList', (req, res) => {
+    res.render('KYW/chatList.ejs')
+});
+
+router.get('/newEnroll', (req, res) => {
+    res.render('KYW/enroll.ejs')
+});
+
+router.post('/forLogin', async (req, res) => {
+    console.log(req.body)
+    const loginUser = await user.findOne({id: req.body.id, pw : req.body.pw});
+
+    req.session.user = {
+        id: loginUser.id,
+    };
+
+    res.render('KYW/applicate.ejs')
 });
 
 

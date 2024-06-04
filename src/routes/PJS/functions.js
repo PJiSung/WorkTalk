3b1ee2c25d3c75c41c6cfdd86a6fb79c4a9f34dd
PJS <starcr222@naver.com>
2024-06-04 10:56:46 +0900

    사원 이미지 등록,수정 백(public/upload 하위에 파일 저장)

--- src/routes/PJS/functions.js
+++ src/routes/PJS/functions.js
@@ -6,14 +6,15 @@ const algorithm = process.env.ALGORITHM;
 const key = Buffer.from(process.env.KEY, 'base64');
 const iv = Buffer.from(process.env.IV, 'base64')
 
+//암호화
 const encrypt = (data, type) => {
     if(type == 'bcrypt'){
-        //bcrypt 암호화
+        //bcrypt 
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(data.split('-')[0], salt);
         return hash;
     } else {
-        //crypto 암호화
+        //crypto 
         let cipher = crypto.createCipheriv(algorithm, key, iv);
         let encrypted = cipher.update(data, 'utf8', 'hex');
         encrypted += cipher.final('hex');
@@ -21,15 +22,15 @@ const encrypt = (data, type) => {
     }
 }
 
+//crypto 복호화
 const decrypt = (data) => {
-    //crypto 복호화
     let decipher = crypto.createDecipheriv(algorithm, key, iv);
     let decrypted = decipher.update(data, 'hex', 'utf8') + decipher.final('utf8');
     return decrypted;
 }
 
+//date 형변환
 const convertDate = (data) =>{
-    //date 형변환
     if(!data) return '';
     return data.toISOString().split('T')[0];
 }
--- src/routes/PJS/login.js
+++ src/routes/PJS/login.js
@@ -4,7 +4,21 @@ const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
 const Employee = require('../../models/employee');
 const bcrypt = require('bcrypt')
-const {encrypt, decrypt, convertDate} = require('./functions')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+const { encrypt, decrypt, convertDate } = require('./functions')
+
+const upload = multer({
+    storage : multer.diskStorage({
+        filename(req, file, done){
+            done(null, req.body.empNo + path.extname(file.originalname))
+        },
+        destination(req, file, done){
+            done(null, path.join(__dirname, '..', '..', "public/upload"))
+        },
+    })
+})
 
 router.post('/login', async (req, res) => {
 
@@ -22,34 +36,34 @@ router.post('/login', async (req, res) => {
             const accessToken = jwt.sign({
                 empNo: employee.empNo,
             }, process.env.ACCESS_SECRET, {
-                expiresIn : "1m",
-                issuer : "workTalk"
+                expiresIn: "1m",
+                issuer: "workTalk"
             });
 
             //refresh Token 발급
             const refreshToken = jwt.sign({
                 empNo: employee.empNo,
             }, process.env.REFRESH_SECRET, {
-                expiresIn : "24h",
-                issuer : "workTalk"
+                expiresIn: "24h",
+                issuer: "workTalk"
             });
 
             res.cookie("accessToken", accessToken, {
-                secure : false,
-                httpOnly : true
+                secure: false,
+                httpOnly: true
             })
 
             res.cookie("refreshToken", accessToken, {
-                secure : false,
-                httpOnly : true
+                secure: false,
+                httpOnly: true
             })
 
             res.render('test')
         } else {
-            res.render('index', {msg : "비밀번호가 일치하지 않습니다."})
+            res.render('index', { msg: "비밀번호가 일치하지 않습니다." })
         }
     } else {
-        res.render('index', {msg : "사원번호가 일치하지 않습니다."});
+        res.render('index', { msg: "사원번호가 일치하지 않습니다." });
     }
 });
 
@@ -58,70 +72,80 @@ router.get('/enroll/:empNo?', async (req, res) => {
     const employees = await Employee.find();
     let empInfo = {};
     let date = {};
-    
     //주민번호 복호화
-    for(let i=0; i<employees.length; i++){
+    for (let i = 0; i < employees.length; i++) {
         employees[i].regNumber = decrypt(employees[i].regNumber).substring(0, 8) + '******';
-
-        if(employees[i].empNo == req.params.empNo){
+        if (employees[i].empNo == req.params.empNo) {
             empInfo = employees[i];
             date.hireDate = convertDate(employees[i].hireDate);
             date.outDate = convertDate(employees[i].outDate);
         }
     }
 
-    if(req.params.empNo) res.render("PJS/enroll", {employees, empInfo, date})
-    else res.render("PJS/enroll", {employees})
+    if (req.params.empNo) res.render("PJS/enroll", { employees, empInfo, date })
+    else res.render("PJS/enroll", { employees })
+    // if (req.params.empNo) res.render("PJS/test", { employees, empInfo, date })
+    // else res.render("PJS/test", { employees })
 
 })
 
-router.post('/enroll', async (req, res) => {
+router.post('/enroll', upload.single('picture'), async (req, res) => {
 
-    const { empNo, picture, dept, position, workType, name, address, phoneNumber, homeNumber, email, status, hireDate, outDate } = req.body;
+    const { empNo, dept, position, workType, name, address, phoneNumber, homeNumber, email, status, hireDate, outDate } = req.body;
 
     const employee = new Employee({
         empNo,
         pwd: encrypt(req.body.regNumber.split('-')[0], 'bcrypt'),
-        picture, dept, position, workType, name,
+        picture : empNo + path.extname(req.file.originalname),
+        dept, position, workType, name,
         regNumber: encrypt(req.body.regNumber, 'crypto'),
         address, phoneNumber, homeNumber, email, status, hireDate, outDate
     })
+
     await employee.save();
     res.redirect("/emp/enroll");
 })
 
-router.post('/update', async (req, res) => {
+router.post('/update', upload.single('picture'), async (req, res) => {
 
     const updateEmpInfo = req.body;
-    const existingEmpInfo = await Employee.findOne({empNo : req.body.empNo});
+    const existingEmpInfo = await Employee.findOne({ empNo: req.body.empNo });
     let updateColumn = {};
 
     const empInfoKeys = Object.keys(updateEmpInfo);
-    if(convertDate(existingEmpInfo.hireDate) == updateEmpInfo.hireDate){
+    if (convertDate(existingEmpInfo.hireDate) == updateEmpInfo.hireDate) {
         empInfoKeys.splice(empInfoKeys.indexOf('hireDate'), 1);
     }
-    if((existingEmpInfo.outDate != null && updateEmpInfo.outDate == '') || convertDate(existingEmpInfo.outDate) == updateEmpInfo.outDate){
+    if ((existingEmpInfo.outDate != null && updateEmpInfo.outDate == '') || convertDate(existingEmpInfo.outDate) == updateEmpInfo.outDate) {
         empInfoKeys.splice(empInfoKeys.indexOf('outDate'), 1);
     }
-    if(updateEmpInfo.regNumber.includes('*')){
+    if (updateEmpInfo.regNumber.includes('*')) {
         empInfoKeys.splice(empInfoKeys.indexOf('regNumber'), 1);
-    }else{
+    } else {
         updateEmpInfo.regNumber = encrypt(updateEmpInfo.regNumber, 'crypto');
     }
     empInfoKeys.splice(empInfoKeys.indexOf('empNo'), 1);
+    empInfoKeys.splice(empInfoKeys.indexOf('picture'), 1);
 
-    for(let i=0; i<empInfoKeys.length; i++){
-        const key = empInfoKeys[i];
-        if(updateEmpInfo[key] != existingEmpInfo[key]) updateColumn[key] = updateEmpInfo[key]
+    if(req.file != null && req.file.filename != existingEmpInfo.picture){
+        fs.unlinkSync(__dirname + "../../../public/upload/" + existingEmpInfo.picture);
+        updateColumn.picture = req.file.filename;
     }
-    
-    if(updateColumn != null){
+
+    for (let i = 0; i < empInfoKeys.length; i++) {
+        const key = empInfoKeys[i];
+        if (updateEmpInfo[key] != existingEmpInfo[key]) {
+            updateColumn[key] = updateEmpInfo[key]
+        }
+    }
+
+    if (updateColumn != null) {
         const updateResult = await Employee.findOneAndUpdate(
-            {empNo : req.body.empNo},
-            {$set : updateColumn},
-            {new : true},
+            { empNo: req.body.empNo },
+            { $set: updateColumn },
+            { new: true },
         )
-        if(updateResult) res.redirect("/emp/enroll/" + req.body.empNo);
+        if (updateResult) res.redirect("/emp/enroll/" + req.body.empNo);
         else res.status(404).send("update fail")
     }
 })
